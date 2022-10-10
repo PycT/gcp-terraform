76e9675ba884d398f0160ebe09438968814866b1
Rustem Zakiev <rustem_zakiev@epam.com>
2022-10-10 19:16:15 +0300

    commit 1

--- main.tf
+++ main.tf
@@ -43,6 +43,31 @@ resource "google_compute_subnetwork" "subnet_02" {
     region = var.region
 }
 
+module "firewall_rules" {
+  source       = "terraform-google-modules/network/google//modules/firewall-rules"
+  project_id   = var.project_id
+  network_name = module.vpc.network_name
+
+  rules = [{
+    name                    = "tf-firewall"
+    description             = null
+    direction               = "INGRESS"
+    priority                = null
+    ranges                  = ["0.0.0.0/0"]
+    source_tags             = null
+    source_service_accounts = null
+    target_tags             = null
+    target_service_accounts = null
+    allow = [{
+      protocol = "tcp"
+      ports    = ["80"]
+    }]
+    deny = []
+    log_config = {
+      metadata = "INCLUDE_ALL_METADATA"
+    }
+  }]
+}
 
 terraform {
     backend "gcs" {
