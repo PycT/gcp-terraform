b1d959ff28c0245ab4d4b48f07420866a0abaeaf
Rustem Zakiev <rustem_zakiev@epam.com>
2022-10-11 16:19:37 +0300

    fixes

--- helper.sh
+++ helper.sh
@@ -1,2 +1,5 @@
-terraform import module.instances.google_compute_instance.tf_instance_1 tf-instance-1
-terraform import module.instances.google_compute_instance.tf_instance_2 tf-instance-2
+read -p "Input zone: " zone
+instanceId1=$(gcloud compute instances describe tf-instance-1 --zone $zone --format="json(id)" | jq -r .id)
+instanceId2=$(gcloud compute instances describe tf-instance-2 --zone $zone --format="json(id)" | jq -r .id)
+terraform import module.instances.google_compute_instance.tf_instance_1 instanceId1
+terraform import module.instances.google_compute_instance.tf_instance_2 instanceId2
--- modules/instances/instances.tf
+++ modules/instances/instances.tf
@@ -1,40 +1,49 @@
 # CHECK THE MACHINE TYPE!
 # resource "google_compute_instance" "tf_instance_1" {
-#   name                      = "tf-instance-1"
+#   name = "tf-instance-1"
 #   machine_type = "n1-standard-1"
+#   zone = var.zone
 #   boot_disk {
 #     initialize_params {
 #         image = "debian-cloud/debian-10"
 #     }
 #   } 
 #   network_interface {
-#      network = var.vpcName
-#      subnetwork = "subnet-01"
+#     network = "default"
 #   }
+#   #network_interface {
+#   #   network = var.vpcName
+#   #   subnetwork = "subnet-01"
+#   #}
 #   metadata_startup_script = "metadata_startup_script = <<-EOT   #!/bin/bash   EOT"
 #   allow_stopping_for_update = true
 # }
 
 # resource "google_compute_instance" "tf_instance_2" {
-#   name                      = "tf-instance-2"
+#   name = "tf-instance-2"
 #   machine_type = "n1-standard-1"
+#   zone = var.zone
 #   boot_disk {
 #     initialize_params {
 #         image = "debian-cloud/debian-10"
 #     }
 #   } 
 #   network_interface {
-#      network = var.vpcName
-#      subnetwork = "subnet-02"
+#     network = "default"
 #   }
+#   #network_interface {
+#   #   network = var.vpcName
+#   #   subnetwork = "subnet-02"
+#   #}
 #   metadata_startup_script = "metadata_startup_script = <<-EOT   #!/bin/bash   EOT"
 #   allow_stopping_for_update = true
 # }
 
 #UNCOMENT TASK 4, FILL THE NAME!
 # resource "google_compute_instance" "tf_instance_3" {
-#   name                      = "FILLME"
+#   name = "FILLME"
 #   machine_type = "n1-standard-2"
+#   zone = var.zone
 #   boot_disk {
 #     initialize_params {
 #         image = "debian-cloud/debian-10"
