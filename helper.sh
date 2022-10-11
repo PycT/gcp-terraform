c74cde5b52de4342a78c3b6e8ae2ddf8db6ef385
Rustem Zakiev <rustem_zakiev@epam.com>
2022-10-11 20:49:25 +0300

    finalized

--- helper.sh
+++ helper.sh
@@ -1,4 +1,6 @@
 read -p "Input zone: " zone
+# instanceId1=$(gcloud compute instances describe tf-instance-1 --zone us-east1-b --format="json(id)" | jq -r .id)
+# instanceId2=$(gcloud compute instances describe tf-instance-2 --zone us-east1-b --format="json(id)" | jq -r .id)
 instanceId1=$(gcloud compute instances describe tf-instance-1 --zone $zone --format="json(id)" | jq -r .id)
 instanceId2=$(gcloud compute instances describe tf-instance-2 --zone $zone --format="json(id)" | jq -r .id)
 terraform import module.instances.google_compute_instance.tf_instance_1 $instanceId1
